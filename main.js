caba950db970cfa3b22d8271e8b645bc2353c24e
733325 <733325@sylvaniastudents.org>
2024-01-10 14:25:21 -0500

    Update main.js
    sadfg

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-$(document).ready(() => {});
+$(document).ready(() => {
+
+$('.hint-box').on('click', () => {
+    $('.hint').slideToggle(450);
+})
+
+$('.wrong-answer-one').on('click', () => {
+    $('.wrong-text-one').fadeOut(600);
+})
+$('.wrong-answer-two').on('click', () => {
+    $('.wrong-text-two').fadeOut(600);
+})
+$('.wrong-answer-three').on('click', () => {
+    $('.wrong-text-three').fadeOut(600);
+    $('.frown').
+})
+
+
+
+});
