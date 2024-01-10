a34a68e8aa0febdceecf75c35525a8c5050f9c62
733325 <733325@sylvaniastudents.org>
2024-01-10 14:39:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,15 +6,23 @@ $('.hint-box').on('click', () => {
 
 $('.wrong-answer-one').on('click', () => {
     $('.wrong-text-one').fadeOut(600);
+    $('.frown').show();
 })
 $('.wrong-answer-two').on('click', () => {
     $('.wrong-text-two').fadeOut(600);
+    $('.frown').show();
 })
 $('.wrong-answer-three').on('click', () => {
     $('.wrong-text-three').fadeOut(600);
-    $('.frown').
+    $('.frown').show();
 })
-
+$('.correct-answer').on('click', () => {
+    $('.frown').hide();
+    $('.smiley').show(350);
+    $('.wrong-answer-one').hide(250);
+    $('.wrong-answer-two').hide(250);
+    $('.wrong-answer-three').hide(250);
+})
 
 
 });
